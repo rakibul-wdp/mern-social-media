2d760f9c117fa451beb9f73af5e7b3cf230150ef
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-11 04:35:00 +0600

    create register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ function App() {
   return (
     <>
       <h1 className="text-red-400">hello world</h1>
-      git commit --date='2024-10-04 06:55:00' -m "update readme file"
     </>
   );
 }
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/api/auth/register", formData);
+      alert("User registered successfully");
+    } catch (err) {
+      alert(err.response.data.error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={formData.username}
+        onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;

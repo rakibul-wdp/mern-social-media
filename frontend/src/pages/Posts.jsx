93c98619cf3d899ca6ccc7b7999c01cf9eb7290e
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-17 04:05:00 +0600

    create post page with functionality

---
+++ frontend/src/pages/Posts.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("http://localhost:5000/api/posts");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;

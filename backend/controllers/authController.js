7b220fc8bafcdd4f681bf0390a31c75193fcbf69
rakibul-wdp <rakibul.wdp@gmail.com>
2025-02-12 21:06:40 +0600

    add login functionality in auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,3 +11,21 @@ exports.register = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) throw new Error("User not found");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error("Invalid credentials");
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};

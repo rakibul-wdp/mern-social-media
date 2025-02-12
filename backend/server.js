aed86fc4e855673f63668c30073311fa9c2a22ff
rakibul-wdp <rakibul.wdp@gmail.com>
2025-02-12 21:05:42 +0600

    use the auth route in root server

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))

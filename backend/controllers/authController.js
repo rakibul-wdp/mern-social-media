1d296a8114175ee9187aeff18818af7951c496ef
rakibul-wdp <rakibul.wdp@gmail.com>
2025-02-12 21:02:48 +0600

    create auth controller with register functionality

---
+++ backend/controllers/authController.js
@@ -0,0 +1,13 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({ username, email, password });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};

ac080cca53fd9815f4665433f02e6c184c537428
rakibul-wdp <rakibul.wdp@gmail.com>
2025-02-12 21:10:36 +0600

    add forget password route on auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,5 +4,6 @@ const { register } = require("../controllers/authController");
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
 
 module.exports = router;

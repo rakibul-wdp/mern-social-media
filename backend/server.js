748b7b6c27d8a02727946b7d2a08aea6030916ea
rakibul-wdp <rakibul.wdp@gmail.com>
2025-02-13 16:58:57 +0600

    add post routes on main server file

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
